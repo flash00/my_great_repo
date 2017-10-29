ea8fd0b0a9c3b54c163295c8a743455fc185d27d
flash00 <dexlorraine@gmail.com>
2017-10-29 16:46:00 -0700

    Add Post model and controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Beautiful Places')
+Category.create(name: 'Beautiful Food')
+Category.create(name: 'Beautiful People')
