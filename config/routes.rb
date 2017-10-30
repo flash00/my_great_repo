ae7fcd93c6d6b5418e8ebd318178bbaa644ebda9
flash00 <dexlorraine@gmail.com>
2017-10-29 17:11:53 -0700

    Add post routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'posts#index'
+# get '/posts',             to: 'posts#index'
+# get '/posts/new',         to: 'posts#new'
+# get '/posts/:id',         to: 'posts#show'
+# get '/posts/:id/edit',    to: 'posts#edit'
+# post '/posts',            to: 'posts#create'
+# patch '/posts/:id',       to: 'posts#update'
+# delete '/posts/:id',      to: 'posts#destroy'
+# all in one line
+
+resources :posts
+
 end
